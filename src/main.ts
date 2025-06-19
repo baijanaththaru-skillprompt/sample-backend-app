0432c6d399a6467aa407b7216bbf827866d60832
baijanaththaru-skillprompt <baijanaththaru@skillprompt.com>
2025-06-19 16:19:57 +0545

    time api

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,11 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.get("/time", (req, res) => {
+  const currentTime = new Date().toLocaleTimeString();
+  res.send(`Current server time is: ${currentTime}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
